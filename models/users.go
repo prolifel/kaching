64c36f2a0cff9e487da4d758d64777014498a067
clement <clement.085w@gmail.com>
2023-07-15 09:34:44 +0700

    feat: add basic cahing
    Signed-off-by: clement <clement.085w@gmail.com>

--- models/users.go
+++ models/users.go
@@ -2,8 +2,9 @@ package models
 
 type (
 	UserResponse struct {
-		UserID int64  `json:"user_id" db:"user_id"`
-		Email  string `json:"email" db:"email"`
-		Name   string `json:"name" db:"name"`
+		UserID     int64  `json:"user_id" db:"user_id"`
+		Email      string `json:"email" db:"email"`
+		Name       string `json:"name" db:"name"`
+		DataSource string `json:"data_source"`
 	}
 )
